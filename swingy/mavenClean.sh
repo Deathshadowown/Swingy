27acd18220f75b941fd535baffcdb829cd72ccbc
Deathshadowown <shanervanniekerk@gmail.com>
2020-09-15 15:14:22 +0200

    Good start

---
+++ swingy/mavenClean.sh
@@ -0,0 +1,7 @@
+# $1 = 1;
+# if [$1 == 1]
+# then
+echo  "Clean Successful"
+rm -r target
+# <commands>
+# fi
--- swingy/src/main/java/swingy/App.java
+++ swingy/src/main/java/swingy/App.java
@@ -1,13 +1,25 @@
 package swingy;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!2" );
+        try {            
+            if (args.length > 0 && args.length < 2) {
+                if (args[0].equals("Console")){
+                System.out.println( "You useing the Console");
+                }else if (args[0].equals("Gui")){
+                    System.out.println("You useing the GUI");
+                }else{
+                    System.out.println("Incorrect info given Please use Gui or Console");
+                    System.exit(2);
+                }
+            }else{
+                System.out.println("Please enter a valid command Gui or Console only");
+                System.exit(2);
+            }
+        } catch (Exception e) {
+            System.out.println("unexpected Error");
+        }
     }
 }
